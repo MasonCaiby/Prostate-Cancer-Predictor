224f3fcd0421033f56eed04e1c57d28dde5115db
Meghan Sloan <meghan.m.sloan@gmail.com>
2017-11-20 09:36:00 -0700

    just changed csv name

--- select_genes.py
+++ select_genes.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as import pd
+import pandas as pd
 
 '''
 This script
@@ -10,16 +10,16 @@ This script
 '''
 
 # genes_from_pivot2 preprocessed using datacleaning_pivot/run_pivot.py scripts
-X = pd.read_csv('Prostate-Cancer-Predictor/genes_from_pivot2.csv')
+X = pd.read_csv('genes_from_pivot2.csv', index_col=0)
 X_data = X.values
-y = pd.read_csv('Prostate-Cancer-Predictor/labels_from_pivot2.csv', index_col=0)
+y = pd.read_csv('labels_from_pivot2.csv', index_col=0)
 y = y.set_index('IndividualID')
 
 top_hundred_genes = set()
 for i in range(496):
     top_hundred_genes = top_hundred_genes.union(set(np.argsort(X_data[i])[-100:]))
 
-top_hund_genes = np.array(list(top_hundred_genes))
+top_hundred_genes = np.array(list(top_hundred_genes))
 genes = X.columns.values
 select_genes = genes[top_hundred_genes]
 
@@ -29,13 +29,13 @@ select_df = X[cols]
 select_df
 
 # Get rows of selectdf that are in class 0 and duplicate 8 times
-zeros = selectdf.values[y['Gleason_binary'].values==0]
+zeros = select_df.values[y['Gleason_binary'].values==0]
 eight = np.ones(45)
 eight = eight*8
 eight = eight.astype(int)
 repeat_arr = np.repeat(zeros, eight, axis=0)
 repeat_df = pd.DataFrame(repeat_arr, columns=cols)
-joined = selectdf.append(repeat_df)
+joined = select_df.append(repeat_df)
 
 # Add 0 classes to labels
 y_add_arr = np.zeros(360).reshape(-1,1).astype(int)
@@ -43,5 +43,5 @@ index = repeat_df.index
 y_add = pd.DataFrame(y_add_arr, index=index, columns=['Gleason_binary'])
 balanced_labels = y.append(y_add)
 
-balanced_labels.to_csv('balanced_labels.csv')
-select_df.to_csv('select_genes.csv')
+balanced_labels.to_csv('balanced_labels1.csv')
+joined.to_csv('select_genes1.csv')
