a1f4e84040be43a1cb2958b37974f41484ad9beb
Meghan Sloan <meghan.m.sloan@gmail.com>
2017-11-19 23:01:38 -0700

    changed tree ensemble methods to classifiers

--- sklearn_models.py
+++ sklearn_models.py
@@ -3,9 +3,9 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.metrics import recall_score, precision_score, roc_curve, auc
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from collections import defaultdict
-from roc.py import roc_plot
+from roc import plot_roc
 import matplotlib.pyplot as plt
 from scipy import interp
 from sklearn.cross_validation import KFold
@@ -13,7 +13,7 @@ from sklearn.preprocessing import StandardScaler
 
 
 def logistic_reg (X_train, X_test, y_train, y_test, genes):
-    LR = LogisticRegression(penalty='l1')
+    LR = LogisticRegression(penalty='l2')
     LR.fit(X_train, y_train)
     y_pred = LR.predict(X_test)
     model_score(X_test, y_test, y_pred, 'Logistic Regression', LR)
@@ -21,10 +21,10 @@ def logistic_reg (X_train, X_test, y_train, y_test, genes):
     genes = genes.T
     ind = np.argsort(params)
     top_genes = genes.flatten()[ind.flatten()][-100:]
-    return top_genes, y_pred
+    return set(top_genes), y_pred
 
 def stoch_grad (X_train, X_test, y_train, y_test, genes):
-    SGD = SGDClassifier(penalty='l1')
+    SGD = SGDClassifier(penalty='l2')
     SGD.fit(X_train, y_train)
     y_pred = SGD.predict(X_test)
     model_score(X_test, y_test, y_pred, 'Stochastic Grad', SGD)
@@ -33,32 +33,27 @@ def stoch_grad (X_train, X_test, y_train, y_test, genes):
     genes = genes.T
     ind = np.argsort(params)
     top_genes = genes.flatten()[ind.flatten()][-100:]
-    return top_genes, y_pred
+    return set(top_genes), y_pred
 
 def tree_reg (X_train, X_test, y_train, y_test, genes, threshold=0.7):
-    RFR = RandomForestRegressor()
-    RFR.fit(X_train, y_train)
-    y_pred = RFR.predict(X_test)
-    y_pred[y_pred>threshold]=1
-    y_pred[y_pred<=threshold]=0
-    y_pred = y_pred.reshape(-1,1).astype(int)
-    model_score(X_test, y_test, y_pred, 'Random Forest', RFR)
-    ind = np.argsort(RFR.feature_importances_)
-    genes = genes.T
-    top_tree_genes = genes.flatten()[ind][-100:]
-    return top_tree_genes, y_pred
+    RFC = RandomForestClassifier()
+    RFC.fit(X_train, y_train)
+    y_pred = RFC.predict(X_test)
+    y_pred = y_pred.reshape(-1,1)
+    model_score(X_test, y_test, y_pred, 'Random Forest', RFC)
+    ind = np.argsort(RFC.feature_importances_)
+    top_tree_genes = genes[ind][-100:]
+    return set(top_tree_genes), y_pred
 
 def adaboost(X_train, X_test, y_train, y_test, genes, threshold=0.7):
-    AB = AdaBoostRegressor()
+    AB = AdaBoostClassifier()
     AB.fit(X_train, y_train)
     y_pred = AB.predict(X_test)
-    y_pred[y_pred>threshold]=1
-    y_pred[y_pred<=threshold]=0
     y_pred = y_pred.reshape(-1,1).astype(int)
     model_score(X_test, y_test, y_pred, 'Adaboost', AB)
     ind = np.argsort(AB.feature_importances_)
     top_ada_trees = genes[ind][-100:]
-    return top_ada_trees, y_pred
+    return set(top_ada_trees), y_pred
 
 
 def model_score (X_test, y_test, y_pred, name, regressor):
@@ -73,15 +68,10 @@ def get_gene_list(X):
 def common_genes(log_set, tree_set, ada_set, sgd_trees, max_iter=10):
     gene_set = log_set.intersection(tree_set, ada_set)
     for i in range(max_iter):
-        top_log_genes, log_pred = logistic_reg(X_train, X_test, y_train, y_test, genes)
-        top_tree_genes, tree_pred = tree_reg(X_train, X_test, y_train, y_test, genes, threshold=threshold)
-        top_ada_trees, tree_pred = adaboost(X_train, X_test, y_train, y_test, genes, threshold=threshold)
-        top_sgd_trees, sgd_pred = stoch_grad(X_train, X_test, y_train, y_test, genes)
-
-        log_set = set(top_log_genes)
-        tree_set = set(top_tree_genes)
-        ada_set = set(top_ada_trees)
-        sgd_set = set(top_sgd_trees)
+        log_set, log_pred = logistic_reg(X_train, X_test, y_train, y_test, genes)
+        tree_set, tree_pred = tree_reg(X_train, X_test, y_train, y_test, genes, threshold=threshold)
+        ada_set, ada_pred = adaboost(X_train, X_test, y_train, y_test, genes, threshold=threshold)
+        sgd_set, sgd_pred = stoch_grad(X_train, X_test, y_train, y_test, genes)
 
         # print (log_set.intersection(tree_set, ada_set, sgd_set))
         gene_set = gene_set.intersection(log_set, tree_set, sgd_set)
@@ -106,20 +96,15 @@ if __name__ == '__main__':
     genes = get_gene_list(X)
     threshold = 0.5
 
-    top_log_genes, log_pred = logistic_reg(X_train, X_test, y_train, y_test, genes)
-    top_tree_genes, tree_pred = tree_reg(X_train, X_test, y_train, y_test, genes, threshold=threshold)
-    top_ada_trees, tree_pred = adaboost(X_train, X_test, y_train, y_test, genes, threshold=threshold)
-    top_sgd_trees, sgd_pred = stoch_grad(X_train, X_test, y_train, y_test, genes)
+    # log_set, log_pred = logistic_reg(X_train, X_test, y_train, y_test, genes)
+    # tree_set, tree_pred = tree_reg(X_train, X_test, y_train, y_test, genes, threshold=threshold)
+    # ada_set, ada_pred = adaboost(X_train, X_test, y_train, y_test, genes, threshold=threshold)
+    # sgd_set, sgd_pred = stoch_grad(X_train, X_test, y_train, y_test, genes)
 
-    log_set = set(top_log_genes)
-    tree_set = set(top_tree_genes)
-    ada_set = set(top_ada_trees)
-    sgd_set = set(top_sgd_trees)
+    # gene_count = common_gene_dict(log_set, tree_set, ada_set, sgd_set)
+    # print(gene_count)
 
-    com_genes = [common_genes(log_set, tree_set, ada_set, sgd_set) for i in range(10)]
-    print (com_genes)
-    gene_count = defaultdict(int)
-    for s in com_genes:
-         for gene in s:
-             gene_count[gene]+=1
-    print(gene_count)
+    plot_roc(X_data, y, RandomForestClassifier)
+    plot_roc(X_data, y, AdaBoostClassifier)
+    # plot_roc(X_data, y, SGDClassifier)
+    plot_roc(X_data, y, LogisticRegression)
